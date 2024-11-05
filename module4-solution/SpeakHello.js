6a3bcb1ad34b9c053101f116f79cba1f19543387
Paria Nourikhah <pariiyankh12@gmail.com>
2024-11-05 16:01:22 +0100

    assignment 4

---
+++ module4-solution/SpeakHello.js
@@ -0,0 +1,18 @@
+// STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
+(function(window) {
+
+  // STEP 3: Create an object, called 'helloSpeaker'
+  var helloSpeaker = {};
+
+  // DO NOT attach the speakWord variable to the 'helloSpeaker' object
+  var speakWord = "Hello";
+
+  // STEP 4: Attach the 'speak' function to the helloSpeaker object
+  helloSpeaker.speak = function(name) {
+    console.log(speakWord + " " + name);
+  };
+
+  // STEP 5: Expose the 'helloSpeaker' object to the global scope
+  window.helloSpeaker = helloSpeaker;
+
+})(window);
